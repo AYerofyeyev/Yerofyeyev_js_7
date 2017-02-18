aa724e18d5998466879354a34db56307280d8c96
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-02-18 20:11:34 +0200

    20170218

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(function() {
+  console.log('Подключена локальная версия jQuery через собственный хостинг');
+  $("<div>", {
+    class: "pure-menu pure-menu-horizontal pure-menu-scrollable"
+  }).appendTo("body");
+  $()
+})
