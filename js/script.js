8b8cca2748d37467d8f37ae78071a0c9c4620065
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-02-19 21:39:17 +0200

    20170219

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-var menu = [];
 var i = "";
 var text = "<p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>";
 
@@ -16,8 +15,9 @@ function showtext() {
     case "menu3":
     text = "<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.<br><br> Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>";
   }
-  console.log(text);
 };
+
+var j = "";
 
 $(function() {
   console.log('Подключена версия jQuery 1.12.4 через google');
@@ -46,24 +46,26 @@ $(function() {
               .append(
                 $("<label>Firstname</label>"),
                 $("<input>")
+                  .attr("id", "firstname")
               ),
             $("<div>")
               .addClass("pure-control-group")
               .append(
                 $("<label>Lastname</label>"),
                 $("<input>")
+                  .attr("id", "lastname")
               ),
             $("<div>")
               .addClass("pure-control-group")
               .append(
                 $("<label>Address</label>"),
                 $("<input>")
+                  .attr("id", "address")
               )
             )
         .append(
           $("<button>Show help</button>")
             .addClass("pure-button pure-button-primary")
-            .css({display: "block"})
         )
       )
   )
@@ -135,4 +137,16 @@ $(function() {
     showtext();
     $("p").replaceWith(text);
   });
+
+  $("input").hover(
+    function() {
+      $(this).append(
+        $("<p>")
+        .addClass("tootip")
+        .append("Please provide your firstname.")
+      );
+    },
+    function() {
+      $("input").remove(".tooltip");
+    });
 })
